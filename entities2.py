c2ed12403ada4a9df54775f5ac0a0e5b944c506d
Luis Casas <luisscasas45@gmail.com>
2024-12-30 00:28:48 -0600

    Revert "FEAT/ decoration functions"
    This reverts commit b38681cf40448f7779ac9282463a8da13f36b6c6.

---
+++ entities2.py
@@ -0,0 +1,20 @@
+class Animal:
+    """Animal"""
+    def __init__(self, legs, eyes):
+        self.legs = legs
+        self.eyes = eyes  
+
+class Human(Animal):
+    def __init__(self, hair_color, eye_color):
+        super().__init__(legs=2, eyes=2)
+        self.hair_color = hair_color
+        self.eye_color = eye_color
+    
+class Spider(Animal):
+    def __init__(self, hair_color):
+        super().__init__(legs=8, eyes=8)
+        self.hair_color = hair_color
+
+#class Person(Human):
+#    hair_color = "Cafe"
+ #   eye_color = "Cafe"
